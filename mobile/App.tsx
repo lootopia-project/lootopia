ad1c14a7cf3c2492c84b4dc1790192d37b8d85d6
metrike <kevinmetri.pro@gmail>
2025-01-04 15:32:27 +0100

    init projet mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+// import type {PropsWithChildren} from 'react';
+import {
+  // SafeAreaView,
+  // ScrollView,
+  // StatusBar,
+  // StyleSheet,
+  // Text,
+  // useColorScheme,
+  // View,
+} from 'react-native';
+
+import {
+} from 'react-native/Libraries/NewAppScreen';
+
+// Main App Component
+function App(): React.JSX.Element {
+    console.log('App Component');
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>NIQUE TES GROSSES FESSES</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center', // Centre verticalement
+        alignItems: 'center', // Centre horizontalement
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center', // Centre le texte lui-même
+    },
+});
+
+
+
+export default App;
