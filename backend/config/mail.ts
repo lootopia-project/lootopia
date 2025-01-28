eef051411c99a3838dc4846cf18958451417aa92
metrike <kevinmetri.pro@gmail>
2025-01-29 00:42:35 +0100

    début mail

--- backend/config/mail.ts
+++ backend/config/mail.ts
@@ -1,34 +1,35 @@
-import { defineConfig, transports } from '@adonisjs/mail'
 import env from '#start/env'
-import { InferMailers } from '@adonisjs/mail/types'
+import { defineConfig, transports } from '@adonisjs/mail'
+import {InferMailers} from "@adonisjs/mail/types";
 
 const mailConfig = defineConfig({
-  /**
-   * Mailer par défaut (doit correspondre à un nom dans la liste des mailers ci-dessous)
-   */
-  mailer: 'smtp', // Déclarez "smtp" comme mailer par défaut
+  default: 'smtp',
 
   /**
-   * Liste des mailers
+   * The mailers object can be used to configure multiple mailers
+   * each using a different transport or same transport with different
+   * options.
    */
   mailers: {
     smtp: transports.smtp({
-      host: env.get('SMTP_HOST', 'smtp.gmail.com'), // Hôte SMTP
-      port: Number(env.get('SMTP_PORT', 587)), // Port SMTP
-      secure: false, // TLS (STARTTLS pour le port 587)
-      auth: {
-        user: env.get('SMTP_USERNAME', ''), // Nom d'utilisateur SMTP
-        pass: env.get('SMTP_PASSWORD', ''), // Mot de passe SMTP
-        type: 'login', // Type d'authentification
-      },
-    }),
-  },
+      host: env.get('SMTP_HOST'),
+      port: env.get('SMTP_PORT'),
+      secure: true,
 
-  /**
-   * Configuration pour les templates
-   */
-  views: {
-    engine: 'edge', // Utilisation du moteur Edge pour les templates d'emails
+      auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME') ?? '',
+        pass: env.get('SMTP_PASSWORD') ?? '',
+      },
+      tls: {},
+
+      ignoreTLS: false,
+      requireTLS: false,
+
+      pool: false,
+      maxConnections: 5,
+      maxMessages: 100,
+    }),
   },
 })
 
