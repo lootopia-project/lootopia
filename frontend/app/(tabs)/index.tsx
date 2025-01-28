421a6ec317adb99eb5735c2d146c614287d40749
antomth <anthonymathieu21@live.fr>
2025-01-28 16:51:12 +0100

    CI end

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <>
     <View>
-      <Text>Hello World</Text>
-      <Link href={'/Truc'}>Truc</Link>
+      <Text>Hello World !</Text>
     </View>
     </>
   );
