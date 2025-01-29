2c22d1987a102011ecbaaa221c65f06490d490a3
metrike <kevinmetri.pro@gmail>
2025-01-29 17:33:15 +0100

    création service mail

--- backend/app/controllers/notifications_controller.ts
+++ backend/app/controllers/notifications_controller.ts
@@ -4,10 +4,6 @@ import MailService from "../service/mail_service.js";
 
 export default class NotificationsController {
     async sendMail() {
-        // console.log("sendMail")
-        // const users:User=await db.from('users').where('nickname', 'kevin').first()
-        //
-        // await MailService.sendMail("recompenses", users)
 
     }
 }
