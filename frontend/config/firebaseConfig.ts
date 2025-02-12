d766795bc874a618deb10a40fb953fdcb388df5b
metrike <kevinmetri.pro@gmail>
2025-02-12 19:40:32 +0100

    correction ci front

--- frontend/config/firebaseConfig.ts
+++ frontend/config/firebaseConfig.ts
@@ -1,12 +1,3 @@
-console.log("📢 Vérification de firebaseConfig :");
-console.log("PROJECT_ID:", process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID);
-console.log("DATABASE_URL:", process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL);
-
-if (!process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || !process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL) {
-    console.error("⚠️ ERREUR : Certaines variables Firebase sont manquantes !");
-    throw new Error("Firebase config is missing required values.");
-}
-
 export const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
